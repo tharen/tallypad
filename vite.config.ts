384d90dc5cc26b8b1fc73d475b97cacb308f2f40
Tod Haren <tharen@users.noreply.github.com>
2026-06-01 08:56:55 -0700

    Fixup Github pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,15 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig(() => {
   // Use root base locally and /tallypad/ for GitHub Pages deploys.
-  const base = process.env.GITHUB_ACTIONS ? '/tallypad/' : '/';
+  // const base = process.env.GITHUB_ACTIONS ? '/tallypad/' : '/';
+  const app = 'dev';
+  if (process.env.GITHUB_REF_NAME) {
+    const app = process.env.GITHUB_REF_NAME === 'main' ? 'app' : process.env.GITHUB_REF_NAME;
+  }
+  const root = '/tallypad';
 
   return {
-    base,
+    base: root,
     plugins: [
       vue(),
       tailwindcss()
